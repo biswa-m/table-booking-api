0dd64fd450239968ea7979c1468bb8601d388dc4
biswa-m <biswa12jit@gmail.com>
2019-01-18 17:36:30 +0530

    Change route to get customer data by phone no

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ router.get('/ping', function(req, res, next){
 });
 
 // user signup, login, view user data, modify user data
-router.use('/user', require('./user/customer'));
-router.use('/user', require('./user/restaurant'));
+router.use('/user', require('./user'));
 
 // Create, update, read and delete restaurant by restaurant owner
 router.use('/restaurant', require('./restaurant'));
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 var router = require('express').Router();
 
-var auth = require('../../helpers/auth');
+var auth = require('../helpers/auth');
 
 var Customer = mongoose.model('Customer');
+var RestaurantOwner = mongoose.model('RestaurantOwner');
 
 /*
  * Create user for new customers
@@ -89,6 +90,31 @@ router.post('/login', function(req, res, next) {
 			return res.status(401).json({errors: 'Wrong email or password'});
 		};
 		
+		return res.json({user: user.toAuthJSON()});
+	}).catch(next);
+});
+
+/*
+ * Restaurant owner login
+ * required data: email, password
+ */
+router.post('/restaurant/login', function(req, res, next) {
+	if (!req.body.user) {
+		return res.sendStatus(400);
+	}
+	if (!req.body.user.email) {
+	    return res.status(422).json({errors: {email: "can't be blank"}});
+	}
+	if (!req.body.user.password) {
+	    return res.status(422).json({errors: {password: "can't be blank"}});
+	}
+
+	RestaurantOwner.findOne({'email': req.body.user.email}).then(function(user) {
+		if (!user) return res.status(401).json({errors: 'Wrong email or password'});
+		if (!user.validPassword(req.body.user.password)) {
+			return res.status(401).json({errors: 'Wrong email or password'});
+		};
+
 		return res.json({user: user.toAuthJSON()});
 	}).catch(next);
 });
