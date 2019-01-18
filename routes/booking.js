0294e1f3bb74a50e5bb1d952573880272035fb5b
biswa-m <biswa12jit@gmail.com>
2019-01-18 16:38:50 +0530

    Create endpoint to get bookings by customer

--- routes/booking.js
+++ routes/booking.js
@@ -5,7 +5,13 @@ var auth = require('../helpers/auth');
 
 var Booking = mongoose.model('Booking');
 var Customer = mongoose.model('Customer');
+var RestaurantOwner = mongoose.model('RestaurantOwner');
+var Restaurant = mongoose.model('Restaurant');
 
+/*
+ * Create a new booking by logged-id customer
+ * Required data: authontication token, restaurant, table, noOfPersons, bookingFrom, bookingTo
+ */
 router.post('/', auth.required, function(req, res, next) {
 	Customer.findById(req.user.id).then(function(customer) {
 		if (!customer) return res.sendStatus(401);
@@ -18,11 +24,29 @@ router.post('/', auth.required, function(req, res, next) {
 		booking.tables = req.body.booking.tables;
 		booking.noOfPersons = req.body.booking.noOfPersons;
 		booking.bookingFrom = req.body.booking.bookingFrom;
-		booking.bookingTo = req.body.booking.bookingTo;
-
-		booking.save().then(function() {
+		booking.bookingTo = req.body.booking.bookingTo; booking.save().then(function() {
 			console.log('Database Updated');
 			return res.json({booking: booking.toUserJSON()});
+		}).catch(next);
+	}).catch(next);
+});
+
+/*
+ * Get all bookings of the logged-in customer
+ * Required data: Authentication token for customer
+ */
+router.get('/', auth.required, function(req, res, next) {
+	Customer.findById(req.user.id).then(function(customer) {
+		if (!customer) return res.sendStatus(401);
+
+		// Get booking data of the customer
+		Booking.find({customer: req.user.id}).then(function(bookings) {
+			var bookingList = [];
+			bookings.forEach(function(booking) {
+				bookingList.push(booking.toUserJSON());
+			});
+
+			res.json({bookings: bookingList});
 		}).catch(next);
 	}).catch(next);
 });
