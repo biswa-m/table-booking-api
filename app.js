91141389d1f012369d444cd2b2d008bba8fda07f
biswa-m <biswa12jit@gmail.com>
2019-01-23 13:17:57 +0530

    Allow access for Cross-origin resource sharing

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ var Booking = require('./models/Booking');
 var app = express();
 
 // Middlewares
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
