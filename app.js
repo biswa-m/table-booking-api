57e6869ba1c6665ae1392eed83adb199785a63e2
neo <biswa12jit@gmail.com>
2019-01-31 00:29:16 +0530

    Set allowed methods for CORS

--- app.js
+++ app.js
@@ -18,17 +18,21 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+  res.header("Access-Control-Allow-methods", "POST, GET, PUT, DELETE");
   next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(function(req, res, next) {//Get rid of this
-	console.log('headers: ', req.headers);
-	console.log('body = ', req.body);
-	console.log('\n\n\n\n');
-	next();
-});
+
+if (config.envName === 'staging') {
+	app.use(function(req, res, next) {//Get rid of this
+		console.log('headers: ', req.headers);
+		console.log('body = ', req.body);
+		console.log('\n\n\n\n');
+		next();
+	});
+}
 
 // Connect database
 mongoose.connect(config.mongodb.URL, config.mongodb.option)
